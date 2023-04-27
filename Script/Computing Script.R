45427ae3330031be505e3fda0a314a7f5364d6fa
Tanaka Takawira <T.Takawira@npt.gov.uk>
2023-04-27 12:18:41 +0100

    Finished computing script

--- Script/Computing Script.R
+++ Script/Computing Script.R
@@ -1,8 +1,24 @@
 # This computing script calculates work done in the Questions script 
 
+# Bring answers given from questions script in memory
 source("Script/Questions.R")
 
-exists("A1")
+
+#Define variables to setup a nice table summary
+Questions <- c("Q1","Q2")
+
+#pull through answers from questions script
+Answers <- c(A1,A2)
 
 
-is.character(A2)
+#Check answers were correct and define the check
+
+C1 <- exists("A1")
+
+
+C2 <- is.character(A2)
+
+Output <- data.frame("questions"=Questions, "answers"=Answers,"Checks" = as.logical(c(C1,C2)) )
+
+
+write.csv(x = Output,file ="Output/output.csv")
